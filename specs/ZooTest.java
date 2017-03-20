f34f8025f5b21911ed096b7487671ae5947e5538
Leon-Paul Hart <leon.paul.hart@gmail.com>
2017-03-20 09:34:52 +0000

    ZooTest.java addCreatureTest passed.

--- specs/ZooTest.java
+++ specs/ZooTest.java
@@ -5,15 +5,23 @@ import zoo.*;
 public class ZooTest {
 
   Zoo zoo;
+  Creature creature;
 
   @Before
   public void before() {
     zoo = new Zoo( "Zizis Zodiac Ziggarut!" );
+    creature = new Creature( "Morchock" , "Dragon" , "Fire" , true , true );
   }
 
   @Test
-  public void getNameTest() {
+  public void getZooNameTest() {
     assertEquals( "Zizis Zodiac Ziggarut!" , zoo.getName() );
+  }
+
+  @Test
+  public void addCreatureTest() {
+    zoo.addCreature(creature);
+    assertEquals( 1 , zoo.countCreatures() );
   }
 
 }
--- zoo/Zoo.java
+++ zoo/Zoo.java
@@ -1,14 +1,25 @@
 package zoo;
+import java.util.*;
 
 public class Zoo {
-  private String name;
+  String name;
+  ArrayList<Creature> creatures;
 
   public Zoo( String name ) {
     this.name = name;
+    this.creatures = new ArrayList<Creature>();
   }
 
   public String getName() {
     return this.name;
+  }
+
+  public void addCreature(Creature creature) {
+    creatures.add(creature);
+  }
+
+  public int countCreatures() {
+    return creatures.size();
   }
 
 }
