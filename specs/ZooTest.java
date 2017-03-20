6ab7645575f41b15d940d4b1f845d3e846486fd2
Leon-Paul Hart <leon.paul.hart@gmail.com>
2017-03-20 09:50:44 +0000

    ZooTest.java addEnclosureTest passed.

--- specs/ZooTest.java
+++ specs/ZooTest.java
@@ -6,11 +6,13 @@ public class ZooTest {
 
   Zoo zoo;
   Creature creature;
+  Enclosure enclosure;
 
   @Before
   public void before() {
     zoo = new Zoo( "Zizis Zodiac Ziggarut!" );
     creature = new Creature( "Morchock" , "Dragon" , "Fire" , true , true );
+    enclosure = new Enclosure( "Volcanic Cave" , "Fire" , 3 , "Morchock" );
   }
 
   @Test
@@ -22,6 +24,12 @@ public class ZooTest {
   public void addCreatureTest() {
     zoo.addCreature(creature);
     assertEquals( 1 , zoo.countCreatures() );
+  }
+
+  @Test
+  public void addEnclosureTest() {
+    zoo.addEnclosure(enclosure);
+    assertEquals( 1 , zoo.countEnclosures() );
   }
 
 }
--- zoo/Zoo.java
+++ zoo/Zoo.java
@@ -4,10 +4,12 @@ import java.util.*;
 public class Zoo {
   String name;
   ArrayList<Creature> creatures;
+  ArrayList<Enclosure> enclosures;
 
   public Zoo( String name ) {
     this.name = name;
     this.creatures = new ArrayList<Creature>();
+    this.enclosures = new ArrayList<Enclosure>();
   }
 
   public String getName() {
@@ -20,6 +22,14 @@ public class Zoo {
 
   public int countCreatures() {
     return creatures.size();
+  }
+
+  public void addEnclosure(Enclosure enclosure) {
+    enclosures.add(enclosure);
+  }
+
+  public int countEnclosures() {
+    return enclosures.size();
   }
 
 }
