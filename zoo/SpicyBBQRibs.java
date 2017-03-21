c97b2f5d6fb565aaaf1d2294e79cdb73023857a9
Leon-Paul Hart <leon.paul.hart@gmail.com>
2017-03-21 16:59:26 +0000

    Minor Changes

---
+++ zoo/SpicyBBQRibs.java
@@ -0,0 +1,15 @@
+package zoo;
+
+public class SpicyBBQRibs implements FoodTypes {
+
+  private String type;
+
+  public SpicyBBQRibs() {
+    this.type = "Smokey!";
+  }
+
+  public String type() {
+    return this.type;
+  }
+
+}
